4ee89497d76b9dc28e7f6c0bf8d647e6a1f4c048
Jan Kraus <kraus.jan@gmail.com>
2016-02-21 22:28:56 +0100

    Adds basic actions & reducers, tweak structure

---
+++ src/reducers/reducers.js
@@ -0,0 +1,26 @@
+import { combineReducers } from 'redux'
+
+import { ADD_TODO } from '../actions/actions.js'
+
+// todos reducer
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return Object.assign({}, state, {
+        ...state,
+        {
+          text: action.text,
+          done: false
+        }
+      ];
+    default:
+      return state;
+  }
+}
+
+// combine multile reducers
+const todoApp = combineReducers({
+  todos
+})
+
+export default todoApp;
